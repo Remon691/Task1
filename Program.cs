7bbef91191b7aac3fedbc4c6122bd3d2b2dbce9c
Ramy <Ramy@DESKTOP-FKGFAP3>
2026-01-12 21:06:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Number of small carpet ");
+int numSmall = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number of  Large carpet");
+int numLarge = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("The small carpet = 25$");
+Console.WriteLine("The large carpet = 35$");
+Console.WriteLine("Sales tax rate is 6% Estimates are valid for 30 days");
+double totalCost = (25 * numSmall + 35 * numLarge) ;
+Console.WriteLine($"The cost:{(totalCost):C}");
+double totalTax = totalCost * (6.0/100);
+Console.WriteLine($"The tax:{(totalTax):C}");
+Console.WriteLine($"Total Estimates : {(totalCost + totalTax):C}");
+
+
